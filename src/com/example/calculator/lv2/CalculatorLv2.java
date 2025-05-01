008eac1b0a23964b9979dfefc4868c1abee7f2fd
osll0 <osll3765@gmail.com>
2025-05-01 21:07:30 +0900

    feat: (Lv.2) 사칙연산 클래소로 구현, 결과 저장 기능 추가

---
+++ src/com/example/calculator/lv2/CalculatorLv2.java
@@ -0,0 +1,37 @@
+package com.example.calculator.lv2;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class CalculatorLv2 {
+    private final Queue<Long> results = new LinkedList<>();
+
+    public long calculate(long a, long b, char operator) {
+        long result;
+        switch (operator) {
+            case '+' -> result = a + b;
+            case '-' -> result = a - b;
+            case '*' -> result = a * b;
+            case '/' -> {
+                if (b == 0) throw new ArithmeticException("0으로 나눌 수 없습니다!");
+                result =  a / b;
+            }
+            default -> throw new IllegalArgumentException("올바르지 못한 연산 기호입니다!");
+        }
+        results.add(result);
+        return result;
+    }
+
+    public Queue<Long> getResults() {
+        return new LinkedList<>(results);
+    }
+
+    public void setResults(Queue<Long> newResults) {
+        results.clear();
+        results.addAll(newResults);
+    }
+
+    public void removeOldestResult() {
+        if (!results.isEmpty()) results.poll();
+    }
+}
